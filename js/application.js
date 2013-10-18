f6d73d9710697d08c87a5793105f31d331c437ac
Andrew Dzhoha <andrew.djoga@gmail.com>
2013-10-18 18:01:37 +0300

    New layout

---
+++ js/application.js
@@ -0,0 +1,23 @@
+$(function () {
+  var activities = $('ul.activities')
+  var tagTitle = $('.tag-title')
+
+  var filter = function (activity) {
+    $('.activity', activities).hide()
+    $('.activity.' + activity, activities).show()
+  }
+
+  var setTitle = function (activity) {
+    tagTitle.html(activity)
+  }
+
+  $(window).bind('hashchange', function () {
+    var hash = window.location.hash.slice(1)
+    if (!$.isEmptyObject(hash)) {
+      filter(hash)
+      setTitle(hash)
+    }
+  })
+
+  $(window).trigger('hashchange')
+})
